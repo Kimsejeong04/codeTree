db5087fba75f1e6f61dc0e6249de09046ab2f91f
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-01-12 17:04:22 +0900

    Title: DateTime to DateTime, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 260112/DateTime to DateTime/datetime-to-datetime.js
+++ 260112/DateTime to DateTime/datetime-to-datetime.js
@@ -3,35 +3,36 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [A, B, C] = input[0].split(' ').map(Number);
 
-let month = 11;
+//let month = 11;
 let day = 11;
 let hour = 11;
 let min = 11;
 let time = 0;
 
-while (true) {
-    if (day === A && hour === B && min === C) {
-        //console.log(time);
-        break;
+const startTimeVal = 11 * 24 * 60 + 11 * 60 + 11;
+const targetTimeVal = A * 24 * 60 + B * 60 + C;
+
+if (targetTimeVal < startTimeVal) {
+    console.log(-1);
+} else {
+    while (true) {
+        if (day === A && hour === B && min === C) {
+            break;
+        }
+
+        time++;
+        min++;
+
+        if (min === 60) {
+            min = 0;
+            hour++;
+        }
+
+        if (hour === 24) {
+            hour = 0;
+            day++;
+        }
     }
 
-    if (day > A) {
-        console.log(-1);
-        break;
-    }
-
-    time += 1;
-    min += 1;
-
-    if (min === 60) {
-        min = 0;
-        hour += 1;
-    }
-
-    if (hour === 24) {
-        hour = 0;
-        day += 1;
-    }
+    console.log(time);
 }
-
-console.log(time);
