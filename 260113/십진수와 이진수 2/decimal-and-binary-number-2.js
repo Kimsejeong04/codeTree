914d6fd82acf4a294fc836e3e1b49a7d11bb9942
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-01-13 21:05:39 +0900

    Title: 십진수와 이진수 2, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

--- 260113/십진수와 이진수 2/decimal-and-binary-number-2.js
+++ 260113/십진수와 이진수 2/decimal-and-binary-number-2.js
@@ -5,8 +5,30 @@ const binaryInput = input[0];
 let num = 0;
 let diameter = 0;
 
-for (let i = 0; i < binaryInput.lenght; i++) {
-    num = num * 2 + binaryInput[i];
-    diameter = num * 17;
+for (let i = 0; i < binaryInput.length; i++) {
+    num = num * 2 + Number(binaryInput[i]);
 }
-console.log(diameter);
+diameter = num * 17;
+
+let digits = [];
+
+while (true) {
+    if (diameter < 2) {
+        digits.push(diameter);
+        break;
+    }
+
+    digits.push(diameter % 2);
+    diameter = Math.floor(diameter / 2);
+}
+
+// 이진수 출력
+let binaryNumber = '';
+for (let i = digits.length - 1; i >= 0; i--) {
+    binaryNumber += digits[i];
+}
+
+console.log(binaryNumber);
+
+
+//console.log(num);
