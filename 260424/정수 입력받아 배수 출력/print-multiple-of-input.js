9893dd768894cd9926fd4658fbc2a1e3417bee86
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-04-24 21:29:32 +0900

    Title: 정수 입력받아 배수 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/정수 입력받아 배수 출력/print-multiple-of-input.js
+++ 260424/정수 입력받아 배수 출력/print-multiple-of-input.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim();
 
-const n = parseInt(input[0]);
-result = [];
+const n = parseInt(input);
+const result = [];
 
 for (let i = 1; i <= 5; i++) {
     result.push(n * i);
