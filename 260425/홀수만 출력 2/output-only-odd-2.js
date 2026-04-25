9b7ac85c507f2521a05d48cda5e1c45aaac0e3ac
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-04-25 22:36:52 +0900

    Title: 홀수만 출력 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260425/홀수만 출력 2/output-only-odd-2.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+const result = [];
+
+for (let i = input[0]; i >= input[1]; i--) {
+    if (i % 2 !== 0) {
+        result.push(i);
+    }
+}
+
+console.log(result.join(" "))
