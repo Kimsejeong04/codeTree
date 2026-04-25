766f073d72a48db9409879205388279b0303974c
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-04-25 22:32:21 +0900

    Title: B부터 A까지 감소, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260425/B부터 A까지 감소/dec-from-b-to-a.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(' ');
+
+const result = [];
+
+for (let i = input[1]; i >= input[0]; i--) {
+    result.push(i);
+}
+
+console.log(result.join(" "));
