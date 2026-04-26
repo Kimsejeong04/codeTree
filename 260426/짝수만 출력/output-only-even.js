41184bd7721bfbe5d6e521c46f3ade9fdf236ad1
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-04-26 15:16:46 +0900

    Title: 짝수만 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260426/짝수만 출력/output-only-even.js
+++ 260426/짝수만 출력/output-only-even.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
 const result = [];
 
-for (let i = result[0]; i <= input[1]; i += 2) {
+for (let i = input[0]; i <= input[1]; i += 2) {
     result.push(i);
 }
 
