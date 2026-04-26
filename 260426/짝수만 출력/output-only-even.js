93139b24b8c7bf9e5536668123d5db1ada6c3708
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-04-26 15:14:53 +0900

    Title: 짝수만 출력, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260426/짝수만 출력/output-only-even.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+const result = [];
+
+for (let i = result[0]; i <= input[1]; i += 2) {
+    result.push(i);
+}
+
+console.log(result.join(" "));
