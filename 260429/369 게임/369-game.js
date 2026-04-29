eadb97596ee19a9451bdb829783ec2d80a9321d1
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-04-29 14:44:59 +0900

    Title: 369 게임, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/369 게임/369-game.js
+++ 260429/369 게임/369-game.js
@@ -2,3 +2,13 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim();
 
 const result = [];
+for (let i = 1; i <= input; i++) {
+    if (i % 3 == 0 || (String(i).includes('3') || String(i).includes('6') || String(i).includes('9'))) {
+        result.push('0');
+    }
+    else {
+        result.push(i);
+    }
+}
+
+console.log(result.join(' '));
