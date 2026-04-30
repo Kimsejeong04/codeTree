c11b4d9c65ec4ce9a5ed3eaed78a2abdd7ab6bed
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-04-30 14:41:29 +0900

    Title: 배수의 개수 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260430/배수의 개수 2/number-of-multipliers-2.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n').map(Number);
+
+let cnt = 0;
+
+for (let i = 0; i < 10; i++) {
+    let current = input[i];
+    if (current % 2 != 0) {
+        cnt++;
+    }
+}
+
+console.log(cnt);
