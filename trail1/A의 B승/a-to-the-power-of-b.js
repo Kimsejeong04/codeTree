f0500ed3dba2359c0299a2b72f6803bd393cf1de
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-06-12 22:54:08 +0900

    [Passed] A의 B승 (69ms, 11MB)

---
+++ trail1/A의 B승/a-to-the-power-of-b.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+let result = 1;
+
+for(let i = 1; i <= input[1]; i++){
+    result *= input[0];
+}
+
+console.log(result)
