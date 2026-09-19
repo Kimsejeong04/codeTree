802164dc1e7768a2a83f17f1f945a25f71d992bd
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2026-09-19 15:46:32 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (59ms, 10MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.js
@@ -0,0 +1,10 @@
+// const fs = require('fs');
+// const input = fs.readFileSync(0).toString().trim();
+
+function printStar() {
+    console.log('**********');
+}
+
+for(let i = 0; i < 5; i++){
+    printStar();
+}
